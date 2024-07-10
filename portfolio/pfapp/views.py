bdaa12f6c20df91c775c1f2e14b9a87025533420
Ivan Tykhonov <tixonoxiwk@gmail.com>
2024-07-10 13:21:20 +0300

    pages + templates

--- portfolio/pfapp/views.py
+++ portfolio/pfapp/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. It worked.")
+    return render(request, template_name='pfapp/index.html')
+
+
+def services(request):
+    return render(request, template_name='pfapp/services.html')
+
+
+def resume(request):
+    return render(request, template_name='pfapp/resume.html')
+
+
+def work(request):
+    return render(request, template_name='pfapp/work.html')
+
+
+def contact(request):
+    return render(request, template_name='pfapp/contact.html')
